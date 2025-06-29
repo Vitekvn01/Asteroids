cc3c94e8a2286492f7e80cc4a4dad37c179b0782
vitekvn01@yandex.ru <vitekvn01@yandex.ru>
2025-06-29 12:20:01 +0700

    upd

--- Assets/Original/Scripts/Infrastructure/Services/AdsController.cs
+++ Assets/Original/Scripts/Infrastructure/Services/AdsController.cs
@@ -1,12 +1,79 @@
+using System.Collections.Generic;
+using AppodealAds.Unity.Api;
+using AppodealAds.Unity.Common;
 using Original.Scripts.Core.Interfaces.IService;
+using UnityEngine;
 
 namespace Original.Scripts.Infrastructure.Services
 {
-    public class AdsController : IAdsController
+    public class AdsController : IAdsController, IInterstitialAdListener, IAppodealInitializationListener
     {
+        private const string AppKey = "1c1e57cb3cdae0b3afde949eafc56230ea3132a142e0b6cb";
+    
+        public IAdsController()
+        {
+            int adTypes = Appodeal.INTERSTITIAL;
+
+            Appodeal.initialize(AppKey, adTypes, this);
+
+            Appodeal.setInterstitialCallbacks(this);
+
+        }
+
         public void ShowInterstitial()
         {
-            throw new System.NotImplementedException();
+            if(Appodeal.isLoaded(Appodeal.INTERSTITIAL)) {
+                Appodeal.show(Appodeal.INTERSTITIAL);
+            }
+            Debug.Log("Show Interstitial");
+        }
+        public void onInterstitialLoaded(bool isPrecache)
+        {
+            Debug.Log("InterstitialLoaded");
+        }
+
+        public void onInterstitialFailedToLoad()
+        {
+            Debug.Log("InterstitialFailedToLoad");
+        }
+
+        public void onInterstitialShowFailed()
+        {
+            Debug.Log("InterstitialShowFailed");
+        }
+
+        public void onInterstitialShown()
+        {
+            Debug.Log("InterstitialShown");
+        }
+
+        public void onInterstitialClosed()
+        {
+            Debug.Log("InterstitialClosed");
+        }
+
+        public void onInterstitialClicked()
+        {
+            Debug.Log("InterstitialClicked");
+        }
+
+        public void onInterstitialExpired()
+        {
+            Debug.Log("InterstitialExpired");
+        }
+
+        public void onInitializationFinished(List<string> errors)
+        {
+            Debug.Log("InitializationFinished");
+
+            if (errors.Count != 0)
+            {
+                int index = 1;
+                foreach (var error in errors)
+                {
+                    Debug.Log($"{index}_error: {error}");
+                }
+            }
         }
     }
 }
