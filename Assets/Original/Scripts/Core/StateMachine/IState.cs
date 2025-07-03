65fba6e13e75aae941ea0f65dd541990138a62fa
vitekvn01@yandex.ru <vitekvn01@yandex.ru>
2025-07-03 20:52:29 +0700

    upd

--- Assets/Original/Scripts/Core/StateMachine/IState.cs
+++ Assets/Original/Scripts/Core/StateMachine/IState.cs
@@ -3,8 +3,8 @@ namespace Original.Scripts.Core.StateMachine
 {
     public interface IState
     {
-        void Enter();
-        void Update();
-        void Exit();
+        public void Enter();
+        public void Update();
+        public void Exit();
     }
 }
